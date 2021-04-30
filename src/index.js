53e5609c0ae3f87684bb1ea69b167c8cbdd0a2c5
thepaj <pavla.jureckova@gmail.com>
2021-04-30 09:39:27 +0200

    fetching random user name and picture

--- src/index.js
+++ src/index.js
@@ -1,8 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+
+const API = 'https://randomuser.me/api/';
+
+const fetchRandomData = async () => {
+  const { data } = await axios.get(API);
+  console.log(data);
+  return data;
+}
+
+class App extends React.Component {
+  state = {
+    randomUser: '',
+    name: '',
+    picture: '',
+  }
+
+  componentDidMount = async () => {
+    const randomUser = await fetchRandomData();
+    const name = randomUser.results[0].name.first + ' ' + randomUser.results[0].name.last;
+    const picture = randomUser.results[0].picture.thumbnail;
+    this.setState({
+      name,
+      picture
+    })
+  }
+
+  render() {
+    const { name, picture } = this.state;
+
+    return(
+      <div>
+        <button onClick={fetchRandomData}>
+          Fetch Random Data
+        </button>
+        <p>
+          {name}
+        </p>
+        <img src={picture} alt="user-thumbnail"/>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +51,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
